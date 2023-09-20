b05fedca2f2261574abccaa92de4081a3eb60b74
gabrielafern4nda <gabriela_f_borges@estudante.sesisenai.org.br>
2023-09-20 07:45:42 -0300

    atualizacao

--- teste.js
+++ teste.js
@@ -1,3 +1,8 @@
 document.getElementById('opcao1').addEventListener('change', function() {
     document.getElementById('opcao1-adicional').style.display = this.checked ? 'block' : 'none';
   });
+
+  document.getElementById('opcao2_nao').addEventListener('change', function() {
+    document.getElementById('opcao2-adicional').style.display = this.checked ? 'block' : 'none';
+  });
+  
