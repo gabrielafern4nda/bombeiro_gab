1ab5686601d13e81670a37eface8e6b4e3ef3b02
gabrielafern4nda <gabriela_f_borges@estudante.sesisenai.org.br>
2023-10-17 12:23:58 -0300

    .

--- a.gestacional/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('opcao1').addEventListener('change', function() {
-    document.getElementById('opcao1-adicional').style.display = this.checked ? 'block' : 'none';
-  });
-
-  document.getElementById('opcao2_nao').addEventListener('change', function() {
-    document.getElementById('opcao2-adicional').style.display = this.checked ? 'block' : 'none';
-  });
-  
